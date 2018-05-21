6a70e16314d51e8e7786ef0e8dcfd06d91004785
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-05-21 11:34:02 -0700

    Add form and dino controller

--- app/controllers/dinos_controller.rb
+++ app/controllers/dinos_controller.rb
@@ -1,6 +1,23 @@
 class DinosController < ApplicationController
   def index
-    
+    @dinos = Dino.all
     
   end
+
+  def new
+    @dino = Dino.new
+    
+  end
+
+  def create
+    @dino = dinos.create(dino_params)
+  end
+
+
+private
+def dino_params
+  params.require(:dino).permit(:name, :description, :diet)
+  
+end
+
 end
