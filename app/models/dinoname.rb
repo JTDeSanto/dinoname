a125104aa23d6cfe6233180bf9e496eab6167ba2
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-05-24 19:13:39 -0700

    More styling, additional form logic and heroku serve assets error debug attempt

--- app/models/dinoname.rb
+++ app/models/dinoname.rb
@@ -2,29 +2,33 @@ class Dinoname
   include ActiveModel::Model
   attr_accessor :name
 
+
   def formatted_name
-    @first_name = self.name
-    @last_name = suffix
-    if /[aeiou]/.match(@first_name.last) && /[aeiou]/.match(@last_name[0])
-      @first_name = @first_name.chomp(@first_name.last)
-    end
-    
+    if self.name.present?
+      @first_name = self.name
+      @last_name = suffix
+      if /[aeiou]/.match(@first_name.last) && /[aeiou]/.match(@last_name[0])
+        @first_name = @first_name.chomp(@first_name.last)
+
+      elsif  /[aeiou]/.match(@first_name.last) == nil && /[aeiou]/.match( @last_name[0])== nil
+         @last_name = 'a' + @last_name
+      end
     
     "#{@first_name}#{@last_name}"
+    
+    else
+      "Iforgotmyname-#{suffix}"
+    end
   end
 
   private
 
   
   def suffix
-    endings = ["saurus","ceratops","dactyl","adon", "alophus" ]
-    if self.name.present?
+    endings = ["saurus","ceratops","dactyl","adon", "alophus","opteryx", "ophodon", "saurus rex", "cephalasaurus" ]
       @y = endings.length - 1
       return "#{endings[rand(0..@y)] }"
         
     end
-
-    
-  end
   
 end
